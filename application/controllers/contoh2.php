1feda2557dd5cee7c522657b71acd6c42650546e
rezabayyy <rezabayu2810@gmail.com>
2023-12-09 00:00:36 +0700

    P01-P12

---
+++ application/controllers/contoh2.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan Diri</h1>";
+        echo "Nama : Reza Bayu Nugroho <br>";
+        echo "Nim  : 122202333 <br>";
+        echo "Kelas: 12.3A.05 <br>";
+        echo "TTL  : Bekasi, 28 Oktober 2004 <br>";
+        echo "Hobi : Bermain Game"; 
+        //contoh 1.php//
+    }
+}
---
+++ application/views/v_footer.php
@@ -0,0 +1,7 @@
+        <footer //vfooter.php// >
+            <a href="http://www.RentalBuku.com">RentalBuku </a>
+            
+        </footer>
+    </div>
+</body>
+</html>
